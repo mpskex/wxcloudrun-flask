86c670ea0bfdfab29f283926dcd6ba9a3c1e41dc
Fangrui Liu <fangruil@moqi.ai>
2023-02-22 16:25:39 +0800

    commit

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -14,10 +14,6 @@ from itsdangerous import url_safe
 import requests
 from os import getenv
 
-# TOKEN = os.getenv("WECHAT_TOKEN", "123456")
-# AES_KEY = os.getenv("WECHAT_AES_KEY", "")
-# APPID = os.getenv("WECHAT_APPID", "")
-
 SITE_NAME = getenv('BACKEND', '')
 API_KEY = getenv('MQ_API_KEY', 'wechat')
 API_SECRET = getenv('MQ_API_SECRET', '')
@@ -26,15 +22,6 @@ auth = url_safe.URLSafeTimedSerializer(API_SECRET, API_KEY)
 
 @app.route('/api/chat', methods=['POST', 'GET'])
 def send():
-    # signature = request.args.get("signature", "")
-    # timestamp = request.args.get("timestamp", "")
-    # nonce = request.args.get("nonce", "")
-    # encrypt_type = request.args.get("encrypt_type", "raw")
-    # msg_signature = request.args.get("msg_signature", "")
-    # try:
-    #     check_signature(TOKEN, signature, timestamp, nonce)
-    # except InvalidSignatureException:
-    #     abort(403)
     if request.method == "GET":
         return ""
     try:
@@ -50,8 +37,8 @@ def send():
         resp = requests.get(f'http://{SITE_NAME}/api/{API_KEY}/{auth.dumps(s)}').content
         resp = json.loads(resp)['message']
     except Exception as e:
-        resp = "API 失败: \n" + traceback.format_exc() + '\n\n' + str(type(resp)) + '\n\n' + str(s)
-    print('resp:\t', resp)
+        resp = "API 失败: \n" + traceback.format_exc() + '\n\n' + str(type(resp)) + resp.decode('utf-8') + '\n\n' + str(s)
+    print('resp:\t', '!' + resp)
     reply = create_reply(resp, snd_msg)
     return reply.render()
 
